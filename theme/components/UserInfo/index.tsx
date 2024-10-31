9041dd8efeeccbd45b0f842baddd0556f1b43dbc
sine <2290512627@qq.com>
2024-10-31 23:58:41 +0800

    feat: antd组件适配暗黑模式

--- theme/components/UserInfo/index.tsx
+++ theme/components/UserInfo/index.tsx
@@ -1,9 +1,9 @@
 import React, {useEffect, useMemo, useState} from 'react';
-import {Button, Card, Flex, Form, Image, Input, message, Modal} from "antd";
+import {Button, Card, ConfigProvider, Flex, Form, Image, Input, message, Modal, theme} from "antd";
 import {useUserInfoStore} from "../../../modal/userInfoStore.ts";
 import {verifyCode} from '../../../lib/bcryptUtils'
 import Auth from "../../../lib/auth.tsx";
-import {normalizeImagePath} from "rspress/runtime";
+import {normalizeImagePath, useDark} from "rspress/runtime";
 
 type FormProps = {
     code: string;
@@ -13,6 +13,7 @@ const UserInfo = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const {currentUserInfo, setCurrentUserInfo} = useUserInfoStore();
     const [form] = Form.useForm();
+    const dark = useDark();
 
 
     const isActive = useMemo(() => (
@@ -50,38 +51,44 @@ const UserInfo = () => {
     return (
         <>
             <Auth/>
-            <Button style={{margin: '0 16px'}} onClick={showModal}>{isActive ? '加入' : '激活'}</Button>
-            <Modal open={isModalOpen} onCancel={handleCancel} footer={!isActive && [
-                <Button key="submit" type="primary" onClick={handleClickSubmit}>
-                    提交
-                </Button>,
-            ]}>
-                <Flex style={{marginTop: 40}} justify='center' gap={20}>
-                    <Card title={'加入我们'}>
-                        <Image
-                            width={100}
-                            src={normalizeImagePath('/group-code.png')}
-                        />
-                    </Card>
-                    <Card title={'1元爱心充电'}>
-                        <Image
-                            width={100}
-                            src={normalizeImagePath('/collection-code.png')}
-                        />
-                    </Card>
-                </Flex>
-                {!isActive && <Flex style={{marginTop: 40}} justify='center'>
-                    <Form
-                        layout={'vertical'}
-                        form={form}
-                        onFinish={handleSubmit}
-                    >
-                        <Form.Item label={'激活码：'} name={'code'}>
-                            <Input.OTP formatter={(str) => str.toUpperCase()}/>
-                        </Form.Item>
-                    </Form>
-                </Flex>}
-            </Modal>
+            <ConfigProvider
+                theme={{
+                    algorithm: dark ? theme.darkAlgorithm:theme.defaultAlgorithm,
+                }}
+            >
+                <Button style={{margin: '0 16px'}} onClick={showModal}>{isActive ? '加入' : '激活'}</Button>
+                <Modal open={isModalOpen} onCancel={handleCancel} footer={!isActive && [
+                    <Button key="submit" type="primary" onClick={handleClickSubmit}>
+                        提交
+                    </Button>,
+                ]}>
+                    <Flex style={{marginTop: 40}} justify='center' gap={20}>
+                        <Card title={'加入我们'}>
+                            <Image
+                                width={100}
+                                src={normalizeImagePath('/group-code.png')}
+                            />
+                        </Card>
+                        <Card title={'1元爱心充电'}>
+                            <Image
+                                width={100}
+                                src={normalizeImagePath('/collection-code.png')}
+                            />
+                        </Card>
+                    </Flex>
+                    {!isActive && <Flex style={{marginTop: 40}} justify='center'>
+                        <Form
+                            layout={'vertical'}
+                            form={form}
+                            onFinish={handleSubmit}
+                        >
+                            <Form.Item label={'激活码：'} name={'code'}>
+                                <Input.OTP formatter={(str) => str.toUpperCase()}/>
+                            </Form.Item>
+                        </Form>
+                    </Flex>}
+                </Modal>
+            </ConfigProvider>
         </>
     );
 };
--- theme/index.tsx
+++ theme/index.tsx
@@ -2,6 +2,7 @@ import Theme from 'rspress/theme';
 import MobileMenuBox from "./components/MobileMenuBox";
 import {Button} from "antd";
 import UserInfo from "./components/UserInfo";
+import '../styles/index.less'
 
 // 以下展示所有的 Props
 const Layout = () => {
