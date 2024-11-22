89545e19bd4bda93e735e3c342d0ebd25531e55a
caihongshun <caihongshun1@minigame.vip>
2024-11-22 10:50:26 +0800

    feat: 添加RevealText组件，统一修改code样式，调整公式字体大小

---
+++ src/components/RevealText.tsx
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import {clsx} from "clsx";
+
+interface RevealTextProps {
+    children: React.ReactNode;
+}
+
+const RevealText: React.FC<RevealTextProps> = (props: RevealTextProps) => {
+    const {children} = props
+    const [isRevealed, setIsRevealed] = useState(false);
+
+    return (
+        <span
+            className={clsx(`relative inline-block border rounded mx-1`, !isRevealed && "min-w-7")}
+            onClick={() => setIsRevealed(true)}
+        >
+      {!isRevealed && (
+          <span className="w-full h-full absolute bg-white cursor-pointer">
+        </span>
+      )}
+            <span className={clsx("inline-block", isRevealed ? "p-1" : "invisible")}>
+        {children}
+      </span>
+    </span>
+    );
+};
+
+export default RevealText;
