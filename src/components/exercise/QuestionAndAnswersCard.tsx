e6d9f4617319e1c170093fbb2879d7f2891aa1b3
october-x <2290512627@qq.com>
2024-11-23 08:19:34 +0800

    feat: 添加QuestionAndAnswersCard组件，并测试

--- src/components/exercise/QuestionAndAnswersCard.tsx
+++ src/components/exercise/QuestionAndAnswersCard.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import SingleSelect from "./SingleSelect";
+import CollapsibleTip from "../../../components/CollapsibleTip";
 
-const QuestionAndAnswersCard = () => {
+type QuestionAndAnswersCardProps = {
+    children?: React.ReactNode;
+    options: React.ReactNode[];
+    currentAnswer: "A"|"B"|"C"|"D";
+};
+
+const QuestionAndAnswersCard = (props: QuestionAndAnswersCardProps) => {
+    const {children,options,currentAnswer} = props;
+    const [value, setValue] = useState<string>()
+    const showCorrectAnswer = true
     return (
         <div>
-
+            <SingleSelect
+                options={options}
+                currentAnswer={currentAnswer}
+                value={value}
+                onChange={setValue}
+                showCorrectAnswer={showCorrectAnswer}
+            />
+            {showCorrectAnswer&&<CollapsibleTip title={"解析"}>{children}</CollapsibleTip>}
         </div>
     );
 };
